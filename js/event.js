205583abe88253f0fe74375391407da8e09e4d40
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-10 11:42:05 +0600

    input field value updated

--- js/event.js
+++ js/event.js
@@ -7,3 +7,14 @@ document.getElementById('event-listener').addEventListener('click', function(){
     const handleText = document.getElementById('handle-text');
     handleText.innerText = 'Change With Add Even Listener';
 })
+
+document.getElementById('update-btn').addEventListener('click', function(){
+
+    const inputFile = document.getElementById('input-field');
+    inputText = inputFile.value;
+
+    const h4 = document.getElementById('update-text');
+    h4.innerText = inputText;
+    
+    inputFile.value = '';
+})
